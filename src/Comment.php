87311c5cf109a694c93480ba43961ba003fcfc89
siekan <siekan@student.edu>
2017-06-12 22:07:22 +0200

    added comment class

--- src/Comment.php
+++ src/Comment.php
@@ -1,100 +1,123 @@
 <?php
 
-
 class Comment {
+    
+    public static function getAllComments(mysqli $conn, $post_id){
+        $sql = "SELECT * FROM Comments WHERE post_id=$post_id ORDER BY creation date DESC";
+        
+        $toReturn = [];
+        
+        $result = $conn->query($sql);
+        if($result !=false){
+            foreach ($result as $row){
+                $comment = new Comment();
+                $comment->id = $row['id'];
+                $comment->user_id = $row['user_id'];
+                $comment->post_id = $row['post_id'];
+                $comment->text = $row['comment_text'];
+                $comment->date = $row['creation_date'];
+                $toReturn[] = $comment;
+            }
+        }
+        
+        return $toReturn;
+    }
+    
+    private $id;
+    private $user_id;
+    private $post_id;
+    private $text;
+    private $date;
+    
+    public function __construct() {
+        $this->id = -1;
+        $this->post_id = null;
+        $this->user_id = null;
+        $this->text = null;
+        $this->date = null;
+    }
+    function getId() {
+        return $this->id;
+    }
 
-private $id;
-private $userId;
-private $tweetId;
-private $creationDate;
-private $text;
+    function getUser_id() {
+        return $this->user_id;
+    }
 
-public function __construct() {
-$this->id = "";
-$this->userId = "";
-$this->tweetId = "";
-$this->creationDate = "";
-$this->text = "";
-}
+    function getPost_id() {
+        return $this->post_id;
+    }
 
-function getId() {
-return $this->id;
-}
+    function getText() {
+        return $this->text;
+    }
 
-function getUserId() {
-return $this->userId;
-}
+    function getDate() {
+        return $this->date;
+    }
 
-function getTweetId() {
-return $this->tweetId;
-}
+    function setId($id) {
+        $this->id = $id;
+    }
 
-function getCreationDate() {
-return $this->creationDate;
-}
+    function setUser_id($user_id) {
+        $this->user_id = $user_id;
+    }
 
-function getText() {
-return $this->text;
-}
+    function setPost_id($post_id) {
+        $this->post_id = $post_id;
+    }
 
-function setUserId($userId) {
-$this->userId = $userId;
-}
+    function setText($text) {
+        $this->text = $text;
+    }
 
-function setTweetId($tweetId) {
-$this->tweetId = $tweetId;
-}
-
-function setCreationDate($creationDate) {
-$this->creationDate = $creationDate;
-}
-
-function setText($text) {
-$this->text = $text;
-}
-
-public function saveToDB(mysqli $conn) {
-if ($this->id == -1) {
-$sql = "INSERT INTO Comments(user_id, tweet_id, creationDate, comment)
-                    VALUES('$this->userId', '$this->tweetId', '$this->creationDate', '$this->text');";
-$result = $conn->query($sql);
-if ($result === TRUE) {
-$this->id = $conn->insert_id;
-return True;
-}
-} else {
-$sql = "UPDATE Comments SET creationDate='$this->creationDate',
-                    comment='$this->text'
-                    WHERE id=$this->id";
-$result = $conn->query($sql);
-if ($result == true) {
-return True;
-}
-}
-return False;
-}
-
-static public function allTweetComments(mysqli $conn, $tweetId) {
-$sql = "SELECT * FROM Comments WHERE tweet_id = $tweetId ORDER BY creationDate DESC";
-$ret = [];
-
-$result = $conn->query($sql);
-if ($result->num_rows != 0) {
-foreach ($result as $row) {
-$comment = new Comment();
-$comment->id = $row['id'];
-$comment->userId = $row['user_id'];
-$comment->tweetId = $row['tweet_id'];
-$comment->creationDate = $row['creationDate'];
-$comment->text = $row['comment'];
-
-$ret[] = $comment;
-}
+    function setDate($date) {
+        $this->date = $date;
+    }
+    
+    public function loadFromDB(){
+        
+        $sql = "SELECT * FROM Comments WHERE post_id=$id";
+        $result = $conn->query($sql);
+        if($result != false) {
+            if($result->num_rows == 1) {
+                $row = $result->fetch_assoc();
+                $this->id = $row['id'];
+                $this->user_id = $row['user_id'];
+                $this->post_id = $row['post_id'];
+                $this->date = $row['creation_date'];
+                $this->text = $row['comment_text'];
+                return true;
+            }
+        }
+    }
+    
+    public function saveCommentToDB (mysqli $conn) {
+        if($this->id == -1) {
+            //insert row to DB
+            $sql = "INSERT INTO Comments (post_id, user_id, comment_text, creation_date) VALUES ('{$this->post_id}', '{$this->user_id}', '{$this->text}', '{$this->date}')";
+            $result = $conn->query($sql);
+            if($result == TRUE) {
+                $this->id = $conn->insert_id;
+                return true;
+            }
+            return false;
+        } else {
+            $sql = "UPDATE Comments SET comment_text = '{$this->text}' WHERE id={$this->id}";
+            $result = $conn->query($sql);
+            return $result;
+        }
+    }
+    
+    public function showComment() {
+        $commentText = $this->getText();
+        return $commentText;
+    }
 }
 
-return $ret;
-}
 
-}
+    
+    
 
 
